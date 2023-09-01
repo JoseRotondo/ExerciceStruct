0b93755fd90c243d10d4d7c867f5ef896ee783fc
Jose_Rotondo <115358292+JoseRotondo@users.noreply.github.com>
2023-09-01 10:16:57 -0300

    Add files via upload

---
+++ ex5.funcao.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+void imprimirnome(){
+
+    
+    printf(" Jose");
+    printf(" Eduardo");
+    printf(" Carlos");
+    printf(" Rotondo");
+}
+int main(){
+    
+    imprimirnome();
+}
